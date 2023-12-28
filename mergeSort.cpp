ddc87685fbef0b514a4e34fe70d06f9c2d7820c0
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-12-28 17:02:42 +0530

    Add files via upload

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,26 +1,30 @@
+// Merge sort in C
+
 #include <stdio.h>
 
-// Merge two subarrays L and R into arr
+// Merge two subarrays L and M into arr
 void merge(int arr[], int p, int q, int r)
 {
-    int n1 = p + q + 1;
+
+
+    int n1 = q - p + 1;
     int n2 = r - q;
 
     int L[n1], R[n2];
 
     for (int i = 0; i < n1; i++)
-    {
         L[i] = arr[p + i];
-    }
-    for (int i = 0; i < n2; i++)
-    {
-        R[i] = arr[q + 1 + i];
-    }
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[q + 1 + j];
+
+    // Maintain current index of sub-arrays and main array
     int i, j, k;
     i = 0;
     j = 0;
     k = p;
 
+    // Until we reach either end of either L or M, pick larger among
+    // elements L and M and place them in the correct position at A[p..r]
     while (i < n1 && j < n2)
     {
         if (L[i] <= R[j])
@@ -36,12 +40,15 @@ void merge(int arr[], int p, int q, int r)
         k++;
     }
 
+    // When we run out of elements in either L or M,
+    // pick up the remaining elements and put in A[p..r]
     while (i < n1)
     {
         arr[k] = L[i];
         i++;
         k++;
     }
+
     while (j < n2)
     {
         arr[k] = R[j];
@@ -50,31 +57,37 @@ void merge(int arr[], int p, int q, int r)
     }
 }
 
-// Divide the array into two subarrays, sort and merge them..
+// Divide the array into two subarrays, sort them and merge them
 void mergeSort(int arr[], int l, int r)
 {
     if (l < r)
     {
+
+        // m is the point where the array is divided into two subarrays
         int m = l + (r - l) / 2;
+
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
-        // merge the sorted subarrays
+
+        // Merge the sorted subarrays
         merge(arr, l, m, r);
     }
 }
+
+// Print the array
 void printArray(int arr[], int size)
 {
     for (int i = 0; i < size; i++)
-        printf("%d", arr[i]);
+        printf("%d ", arr[i]);
     printf("\n");
 }
+
 int main()
 {
     int arr[] = {6, 5, 12, 10, 9, 1};
     int size = sizeof(arr) / sizeof(arr[0]);
-    printf("%d", size);
     mergeSort(arr, 0, size - 1);
-    printf("Sorted array: \n");
 
+    printf("Sorted array: \n");
     printArray(arr, size);
 }
